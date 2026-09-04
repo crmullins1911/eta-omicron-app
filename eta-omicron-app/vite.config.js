9a3bd80c8f4768f2541292624307d40b82efe940
crmullins1911 <cmullins1982@gmail.com>
2026-09-04 12:08:50 -0400

    Update vite.config.js

--- eta-omicron-app/vite.config.js
+++ eta-omicron-app/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
       registerType: "autoUpdate",
       includeAssets: ["icon-192.png", "icon-512.png"],
       manifest: {
